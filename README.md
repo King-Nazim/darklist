9f24224a0a8c91ef93c76eea6e1c5a317c9de23d
Nazim Cp <88269640+King-Nazim@users.noreply.github.com>
2021-09-12 18:04:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Usage
+
+* `pkg install git`
+* `git clone https://github.com/King-Nazim/darklist`
+* `cd darklist`
+* `bash run.sh`
+
+
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sleep 2
+clear
+figlet DrakList
+echo ""
+echo "Developed By $GREEN King-Nazim And MishalMMSS"
+echo ""
+sleep 6
+echo $Red." GENARATING WORDLIST FOR YOU";                                                                                            
+sleep 4
+echo ""
+echo "Ctrl + C to Stop Generating"
+sleep 4                                                                     
+
+string=( {a..z} )
+for x in ${string[@]}; do
+for y in ${string[@]}; do
+for z in ${string[@]}; do
+for w in ${string[@]}; do
+for k in ${string[@]}; do
+#for q in ${string[@]}; do
+echo $x$y$z$w$k
+
+
+done done done done done
+
+echo "Keep Calm And Hack The World"
